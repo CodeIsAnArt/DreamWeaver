73ccf6523ffd086f577746ea601a2b2b864e4787
droidlover <amanjain92@gmail.com>
2018-08-24 10:22:27 +0530

    f

--- js/style.js
+++ js/style.js
@@ -17,12 +17,10 @@ function scrollHorizontal(event) {
 function moveNext(event) {
     isMouseDown = !isMouseDown;
     console.log(isMouseDown);
-    while (isMouseDown) {
-        if (event.type == "mousedown" || event.type == "touchstart") {
-            if (imageSrollIndex <= scrollableWidth) {
-                imageSrollIndex = imageSrollIndex + 20;
-                scrollHorizontal(event);
-            }
+    if (event.type == "mousedown" || event.type == "touchstart") {
+        if (imageSrollIndex <= scrollableWidth) {
+            imageSrollIndex = imageSrollIndex + 20;
+            scrollHorizontal(event);
         }
     }
 }
@@ -30,14 +28,11 @@ function moveNext(event) {
 function movePrevious(event) {
     isMouseDown = !isMouseDown;
     console.log(isMouseDown);
-    isMouseDown
-    if (event.type == "mousedown" || event.type == "touchstart") {
-        mouseDownTimer = setInterval(function () {
-            if (imageSrollIndex >= 0) {
-                imageSrollIndex = imageSrollIndex - 10;
-                scrollHorizontal(event);
-            }
-        }, 100);
+    {
+        if (imageSrollIndex >= 0) {
+            imageSrollIndex = imageSrollIndex - 10;
+            scrollHorizontal(event);
+        }
     }
 }
 
